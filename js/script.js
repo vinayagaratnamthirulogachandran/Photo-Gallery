698310bffa8e3b7ecd68c90158d9c963e785d195
vinayagaratnamthirulogachandran <misterthiru610@gmail.com>
2023-05-16 23:13:49 +0530

    final commit js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-// const model = document.createElement('div');
-// model.id = 'model';
-// document.body.appendChild(model);
+const model = document.createElement('div');
+model.id = 'model';
+document.body.appendChild(model);
 
-// const images = document.querySelectorAll('.img');
+const images = document.querySelectorAll('.img');
 
-// images.forEach(image=>{
-//     image.addEventListener('click', ()=>{
-//         model.classList.add('active');
-//         const img = document.createElement('img');
-//         img.src = image.src;
-//         img.id = "img";
-//         while(model.firstChild){
-//             model.removeChild(model.firstChild);
-//         }
-//         model.append(img);
-//     });
-// });
+images.forEach(image=>{
+    image.addEventListener('click', ()=>{
+        model.classList.add('active');
+        const img = document.createElement('img');
+        img.src = image.src;
+        img.id = "img";
+        while(model.firstChild){
+            model.removeChild(model.firstChild);
+        }
+        model.append(img);
+    });
+});
 
-// model.addEventListener('click', ()=>{
-//     model.classList.remove('active');
-// });
+model.addEventListener('click', ()=>{
+    model.classList.remove('active');
+});
